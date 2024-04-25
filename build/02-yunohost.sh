fd45928cb5247161b5cac666502885cb6ebaf520
Christoph Hummer <christophhummer@gmail.com>
2024-04-25 08:27:55 +0200

    Add files via upload

---
+++ build/02-yunohost.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -o /tmp/install.sh https://install.yunohost.org
+bash /tmp/install.sh -a
---
+++ build/99-cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt clean
+rm -rf /var/cache/apt/archives/* /tmp/install.sh
+
+find /var/log -type f -mtime +30 -exec rm {} \;
+sed -i -e '/Internal IP address/c ***line removed***' /tmp/02-yunhost.log
