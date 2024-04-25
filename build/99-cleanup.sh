4567d5e7b2151663270993deab6fafa73b2549c2
Christoph Hummer <christophhummer@gmail.com>
2024-04-25 08:51:12 +0200

    Update 99-cleanup.sh

--- build/99-cleanup.sh
+++ build/99-cleanup.sh
@@ -3,4 +3,4 @@ apt clean
 rm -rf /var/cache/apt/archives/* /tmp/install.sh
 
 find /var/log -type f -mtime +30 -exec rm {} \;
-sed -i -e '/- https:\/\//c ***line removed***' /tmp/02-yunhost.log
+sed -i -e '/- https:\/\//c ***line removed***' /tmp/02-yunohost.log
